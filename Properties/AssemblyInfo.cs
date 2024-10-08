03ba7704ef7399c97ee2bf2252caa8cb54f5296f
白いねこ <shiroineko.tfs@gmail.com>
2024-10-08 11:04:06 +0700

    Update project with newer csproj and AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,14 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Hyper-V Device Passthrough")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Hyper-V Device Passthrough is a suite to bypass the restriction of Hyper-V security, to let the phyical devices running inside a Hyper-V VM.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TheFlightSims (feat. chanket@github")]
+[assembly: AssemblyCompany("TheFlightSims (feat. chanket@github.com)")]
 [assembly: AssemblyProduct("Hyper-V Device Passthrough")]
-[assembly: AssemblyCopyright("Copyright © 2018 - 2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2018 - 2024")]
+[assembly: AssemblyTrademark("TheFlightSims")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f2a2fc53-f5e9-4350-8d9b-98269df4c9b1")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
